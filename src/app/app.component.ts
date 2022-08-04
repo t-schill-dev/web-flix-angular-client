87bf8ca91adb2dd77c2ef099f77ea5802c61d0d0
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-08-04 11:42:03 +0200

    Move code from app.html to welcome.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 // ROOT COMPONENT
 
 import { Component } from '@angular/core';
-import {UserRegistrationFormComponent} from './user-registration-form/user-registration-form.component';
-import {LoginFormComponent} from './login-form/login-form.component';
-import {MovieCardComponent} from './movie-card/movie-card.component';
-import {MatDialog} from '@angular/material/dialog'
 
 @Component({
   selector: 'app-root',
@@ -13,25 +9,4 @@ import {MatDialog} from '@angular/material/dialog'
 })
 export class AppComponent {
   title = 'web-flix-angular-client';
-
-  constructor(public dialog: MatDialog) {}
-  //Function to open dialog when signup button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      //Assigning width
-      width: '280px'
-    });
-  }
-//opens dialog of Login Component when button clicked
-  openLoginDialog(): void {
-    this.dialog.open(LoginFormComponent, {
-      width: '280px'
-    });
-  }
-
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px'
-    });
-  }
 }
