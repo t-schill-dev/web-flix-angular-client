e5205d7f516477e6280f422bab61de265d7ecb96
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-08-04 16:21:15 +0200

    Add genre and director view

--- src/app/director/director.component.ts
+++ src/app/director/director.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-director',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectorComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Name: string,
+      Bio: string,
+      Birthday: Date,
+    }
+
+  ) { }
 
   ngOnInit(): void {
   }
