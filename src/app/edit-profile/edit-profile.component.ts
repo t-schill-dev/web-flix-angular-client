b9387aa1d8277aaa7d0a2eee54a25adb53b63dab
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-08-05 11:58:59 +0200

    Add content to edit profile component

--- src/app/edit-profile/edit-profile.component.ts
+++ src/app/edit-profile/edit-profile.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
-  styleUrls: ['./edit-profile.component.scss']
+  styleUrls: ['./edit-profile.component.scss'],
 })
 export class EditProfileComponent implements OnInit {
+  @Input() userData: any = {};
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+    public dialogRef: MatDialogRef<EditProfileComponent>,
+    public fetchApiData: FetchApiDataService,
+    public router: Router,
+    public snackBar: MatSnackBar
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  editProfile(): void {
+    this.fetchApiData.updateUser(this.userData).subscribe((result) => {
+      this.dialogRef.close();
+      console.log(result);
+      this.snackBar.open('Sucessfully updated your profile', 'OK', {
+        duration: 1000,
+      });
+    });
+    localStorage.clear();
+    this.snackBar.open('Please log in with your new credentials', 'OK', {
+      duration: 2000
+    })
+    this.router.navigate(['welcome']);
+    
   }
-
 }
