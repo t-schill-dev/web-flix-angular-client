8a3a7a6fdf9612a88e91f57d4d26be1fa292c2e4
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-08-05 11:16:39 +0200

    Create profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {FetchApiDataService} from '../fetch-api-data.service';
+
+import {MatDialog} from '@angular/material/dialog';
+import {MatSnackBar} from '@angular/material/snack-bar'
+
+import {EditProfileComponent} from '../edit-profile/edit-profile.component';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +13,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+user: any = {};
 
-  constructor() { }
+  constructor(
+public fetchApiData: FetchApiDataService,
+public dialog: MatDialog,
+public snackBar: MatSnackBar,
+public router: Router
+
+  ) { }
 
   ngOnInit(): void {
   }
+//Gets userdata from API
+  getUser(): void {
+    this.fetchApiData.getUser().subscribe((result: any) => {
+      this.user = result;
+      return this.user;
+    })
+  };
 
+  openEditDialog(): void {
+this.dialog.open(EditProfileComponent, {
+  width: '3oopx'
+})
+  }
+
+  deleteProfile():void {
+    if(confirm('Are you sure you want to delete your profile?')) {
+      this.router.navigate(['welcome']). then(()=> {
+        this.snackBar.open('Your profile has been deleted', 'OK', {
+          duration: 1000
+        })
+      });
+      // API call to delete profile
+      this.fetchApiData.deleteUser().subscribe(()=> {
+        localStorage.clear()
+      })
+    }
+  }
 }
